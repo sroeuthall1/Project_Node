7acced5428f92cfe0424e174efd3afc9d0673d35
sroeuthall1 <142596996+sroeuthall1@users.noreply.github.com>
2026-03-14 12:46:34 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'mysql://root:zuhoDlPCDFARqRZKRgUzaKfwhlKzECQs@mysql.railway.internal:3306/railway',
+  process.env.DATABASE_URL || 'mysql://root:vVVofCRvopQoBmETNsCIOqMgkTSJkzeE@mysql.railway.internal:3306/railway',
   {
     dialect: 'mysql'
   }
